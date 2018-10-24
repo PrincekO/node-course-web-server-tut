a4e446fe7d00425962b171b0de37f8cd0804e6c1
Prince Ofori <princeofori065@gmail.com>
2018-10-24 11:03:07 +0100

    Projects page added

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page',
+        message: 'Welcome to the place I pour out my ideas'
+    });
+});
+
 app.listen(port, () => {
     console.log(`> Server is up on port ${port}`);
 });
